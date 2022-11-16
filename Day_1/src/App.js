3e4c9a92532dc3f270e65e86e57717c3495abac8
Sumit Gokhale <sumitgokhale@Sumits-MacBook-Air.local>
2022-11-16 23:58:17 +0530

    Code comments added

--- Day_1/src/App.js
+++ Day_1/src/App.js
@@ -1,3 +1,8 @@
+// Create the React element using React object
 const heading = React.createElement("h1", {}, "Hello World with React");
+
+// Create the react application root using ReactDOM object
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render element in react root
 root.render(heading);
